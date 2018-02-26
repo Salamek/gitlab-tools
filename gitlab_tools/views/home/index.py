aeda96742cc8ff94a7f6eb6f688aa0244caf6281
Adam Schubert <adam.schubert@sg1-game.net>
2018-02-26 13:46:46 +0100

    Show fingerprints on homepage

--- gitlab_tools/views/home/index.py
+++ gitlab_tools/views/home/index.py
@@ -1,9 +1,11 @@
 import flask
 import os
-
+import paramiko
 from flask_login import current_user, login_required
 from gitlab_tools.models.gitlab_tools import PullMirror
-from gitlab_tools.tools.helpers import get_user_public_key_path
+from gitlab_tools.tools.helpers import get_user_public_key_path, get_user_private_key_path
+from gitlab_tools.tools.formaters import format_md5_fingerprint, format_sha256_fingerprint
+from gitlab_tools.tools.crypto import import_key, calculate_fingerprint
 from gitlab_tools.blueprints import home_index
 
 __author__ = "Adam Schubert"
@@ -13,11 +15,23 @@ __date__ = "$26.7.2017 19:33:05$"
 @home_index.route('/', methods=['GET'])
 @login_required
 def get_home():
-    mirrors = PullMirror.query.count()
-    public_key_path = get_user_public_key_path(current_user, flask.current_app.config['USER'])
-    if os.path.isfile(public_key_path):
-        with open(public_key_path, 'r') as f:
-            public_key = f.read()
+    pull_mirrors = PullMirror.query.count()
+    private_key_path = get_user_private_key_path(current_user, flask.current_app.config['USER'])
+    if os.path.isfile(private_key_path):
+
+        with open(private_key_path, 'r') as f:
+            public_key = paramiko.RSAKey.from_private_key(f)
+        fingerprint_md5 = format_md5_fingerprint(calculate_fingerprint(public_key, 'md5'))
+        fingerprint_sha256 = format_sha256_fingerprint(calculate_fingerprint(public_key, 'sha256'))
     else:
-        public_key = 'Not yet generated...'
-    return flask.render_template('home.index.home.html', mirrors=mirrors, public_key=public_key)
+        public_key = None
+        fingerprint_md5 = None
+        fingerprint_sha256 = None
+
+    return flask.render_template(
+        'home.index.home.html',
+        pull_mirrors=pull_mirrors,
+        public_key=public_key,
+        fingerprint_md5=fingerprint_md5,
+        fingerprint_sha256=fingerprint_sha256
+    )
