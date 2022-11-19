0c125628b2b2d1ad1655dc76a2ff654bb53ee473
Adam Schubert <adam.schubert@sg1-game.net>
2022-11-19 06:00:53 +0100

    New version 1.3.2

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ else:
 
 setup(
     name='gitlab-tools',
-    version='1.3.1',
+    version='1.3.2',
     python_requires='>=3.7',
     description='GitLab Tools',
     long_description=long_description,
