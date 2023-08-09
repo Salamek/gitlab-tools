058b4c55d105e888894a7a0e470193a2f8521134
Adam Schubert <adam.schubert@sg1-game.net>
2023-08-09 14:05:50 +0200

    New version 1.4.5

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ else:
 
 setup(
     name='gitlab-tools',
-    version='1.4.4',
+    version='1.4.5',
     python_requires='>=3.7',
     description='GitLab Tools',
     long_description=long_description,
